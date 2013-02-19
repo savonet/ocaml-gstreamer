0a10baaf1da26222eff2a5efa8572a41b974ba87
Samuel Mimram <smimram@gmail.com>
2013-02-19 18:39:25 +0100

    Use sensible default sink.

--- examples/sine/sine.ml
+++ examples/sine/sine.ml
@@ -3,7 +3,7 @@ open Gstreamer
 let () =
   init ();
   Printf.printf "%s\n%!" (version_string ());
-  let bin = Pipeline.parse_launch "audiotestsrc ! audioconvert ! audioresample ! osssink" in
+  let bin = Pipeline.parse_launch "audiotestsrc ! audioconvert ! audioresample ! autoaudiosink" in
     ignore (Element.set_state bin Element.State_playing);
     Unix.sleep 5;
     ignore (Element.set_state bin Element.State_null);
